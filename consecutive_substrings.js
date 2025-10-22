7c2cd78db0fe3d1797b5232b74696eb38ad0aaae
Garrett Pfingston <gpfingston@gmail.com>
2025-10-22 15:28:23 -0500

    found substrings

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,5 +1,12 @@
 function consecutiveSubstrings(string) {
   // type your code here
+  const substrings = [];
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i + 1; j <= string.length; j++) {
+      substrings.push(string.slice(i, j));
+    }
+  }
+  return substrings;
 }
 
 if (require.main === module) {
